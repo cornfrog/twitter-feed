5c11b2d0150ab7c1953f1f450dd11ef39a969e6d
CornFrog <thecornfrog@gmail.com>
2023-12-28 14:18:11 -0500

    modifications to components and css file

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,6 +1,11 @@
 import React from "react"
 
 const Tweet = (props) => {
+  // getting tweet post
+  // data from TwitterFeed.js
+  const post = props.post;
+
+  console.log(`Current Tweet id_str: ${post.id_str}`);
   return (
     <div>Tweet Component</div>
   )
--- src/components/TwitterFeed.js
+++ src/components/TwitterFeed.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import Tweet from './Tweet';
 
 const TwitterFeed = (props) => {
+  // getting the data 
+  // passed from main.js
+  const tweetData = props.data;
+  // console.log(`props.data from TwitterFeed.js:\n${tweetData}`);
+  // tweetData.forEach((tweet, index) => {
+    // console.log(`Tweet[${index}] ID => ${tweet.id_str}`)
+  // });
+
+  // map over each individual 
+  // object from tweetData 
+  const tweets = tweetData.map((tweet) => {
+    // create a tweet component
+    // and pass it the currently
+    // mapped tweet data
+    return <Tweet post={tweet}/>
+  });
   return(
     <div>Twitter Feed Component</div>
   )
